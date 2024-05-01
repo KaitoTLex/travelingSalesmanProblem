d9b51a544e90ce0e8c897a1a1a6c4f8915c7784e
KaitoTLex <70042943+KaitoTLex@users.noreply.github.com>
2024-05-01 13:29:25 -0700

    amended main.cpp

--- main.c
+++ main.c
@@ -1,29 +1,11 @@
 /*
-*                   ---Traveling Salesman Problem---
-*   A sus salesman is looking for the most efficient way to traveleing 
-*   door to door to sell his products. The places are selected are 
-*   selected at random and the algorithm must be able to differentiate
-*   the distance between each point. Finding the shortest distance 
-*   from each point. 
-*
-*                           ---Execution---
-*   With the rise of parallel computing such as the Cuda cores, finding
-*   the most shortest length between points become easier. The 
-*   development of finding the differrent distances while calculating
-*   the length of the most efficient route could be done in mere seconds
-*
-*                   ---Program Rules (boundries)---
-*   This is an example of the CUDA and it's capabilities to solve this problem
-*   so basic rules must consists of 
-*   - execution of basic CUDA C++ Functions
-*   - must be able to render in 
+* NIAHHAHAHAHAH                   
 *
 */
 
 
 
 #include <stdio.h>
-#include <iostream>
 #include <math.h>
 
 void progarm(){
